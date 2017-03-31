634835995960b59592061522608861415e6f4342
Kordor92 <jozsefszegletes@gmail.com>
2017-03-31 08:45:31 +0200

    Initial commit

---
+++ app/src/main/java/labor/mobsoft/hu/mobilsoftlab/MainActivity.java
@@ -0,0 +1,13 @@
+package labor.mobsoft.hu.mobilsoftlab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
