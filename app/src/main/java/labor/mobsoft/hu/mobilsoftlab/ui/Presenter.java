6f27087faa65e9a686400a8c758b5a1f6e2788d7
Koszta Ádám <koszta6@gmail.com>
2017-03-31 09:49:42 +0200

    MVP

---
+++ app/src/main/java/labor/mobsoft/hu/mobilsoftlab/ui/Presenter.java
@@ -0,0 +1,17 @@
+package labor.mobsoft.hu.mobilsoftlab.ui;
+
+/**
+ * Created by mobsoft on 2017. 03. 31..
+ */
+
+public abstract class Presenter<S> {
+    protected S screen;
+
+    public void attachScreen(S screen) {
+        this.screen = screen;
+    }
+
+    public void detachScreen() {
+        this.screen = null;
+    }
+}
