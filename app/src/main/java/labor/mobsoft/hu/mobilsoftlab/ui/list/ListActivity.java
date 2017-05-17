fede7b5663f05552e8a1a86c37e185a287e7edbc
Adam Koszta <koszta6@gmail.com>
2017-05-17 11:09:45 +0200

    text change

--- app/src/main/java/labor/mobsoft/hu/mobilsoftlab/ui/list/ListActivity.java
+++ app/src/main/java/labor/mobsoft/hu/mobilsoftlab/ui/list/ListActivity.java
@@ -129,7 +129,7 @@ public class ListActivity extends AppCompatActivity implements ListScreen {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
